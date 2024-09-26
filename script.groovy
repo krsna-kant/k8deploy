b1248354fd06afbd1ecf8415d5183655805efa13
KRSNA-KANT <krishnakantrsharma@gmail.com>
2024-09-26 17:01:10 +0530

    changed url

--- script.groovy
+++ script.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Clone Code') {
             steps {
-                git branch: 'main', url: 'https://github.com/krsna-kant/mark_ecommerce.git'
+                git branch: 'main', url: https://github.com/Ab-D-ev/kubernetes-devops-project.git
             }
         }
     }
