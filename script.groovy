127a38342e56264fcb9f4fa3997df7674fb758e3
KRSNA-KANT <krishnakantrsharma@gmail.com>
2024-09-26 17:41:34 +0530

    adding a second stage

--- script.groovy
+++ script.groovy
@@ -7,6 +7,18 @@ pipeline {
                 git branch: 'main', url: 'https://github.com/Ab-D-ev/kubernetes-devops-project.git'
             }
         }
+        stage('Build Docker Image') {
+            steps {
+                script {
+                    echo 'Building Docker image...'
+                    // Replace the following commands with your actual Docker build and push steps
+                    sh 'docker build -t mark-ecommerce:latest .'
+                }
+            }
+        }
     }
 }
+
+
+
 
