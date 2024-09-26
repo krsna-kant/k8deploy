551454e2228990086751e3fd7c8aa79e12d47b49
KRSNA-KANT <krishnakantrsharma@gmail.com>
2024-09-26 16:50:36 +0530

    first commit

---
+++ script.groovy
@@ -0,0 +1,10 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Clone Code') {
+            steps {
+                git branch: 'main', url: 'https://github.com/krsna-kant/mark_ecommerce.git'
+            }
+        }
+        
