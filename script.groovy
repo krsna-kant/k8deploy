b30bb055a34082638ba6f2c856d2b1b8938b2c35
KRSNA-KANT <krishnakantrsharma@gmail.com>
2024-10-01 17:54:53 +0530

    Commented code for testing

--- script.groovy
+++ script.groovy
@@ -2,35 +2,41 @@ pipeline {
     agent any
 
     stages {
-        stage('Clone Code') {
-            steps {
-                // Clone code from the GitHub repository
-                git branch: 'main', url: 'https://github.com/krsna-kant/K8attempt2.git'
+        // stage('Clone Code') {
+        //     steps {
+        //         git branch: 'main', url: 'https://github.com/krsna-kant/K8attempt2.git'
+        //     }
+        // }
+        // stage('Build Docker Image') {
+        //     steps {
+        //         script {
+        //             echo 'Building Docker image...'
+        //             // Replace the following commands with your actual Docker build and push steps
+        //             sh 'docker build -t mark_to_do:latest .'  
+        //         }
+        //     }
+        // }
+        // stage('Pushing Image to DockerHub'){
+        //         steps{
+        //         withCredentials([string(credentialsId: 'DHPass', variable: 'DHPass')]) {
+        //         sh 'docker login -u krsna3629 -p ${DHPass}'
+        //         sh 'docker image push krsna3629/$JOB_NAME:v1.$BUILD_ID'
+        //         sh 'docker image push krsna3629/$JOB_NAME:latest'
+        //      }
+        //    }
+        // }
+        stage('Sending file to Ansible'){
+            steps{
+                sshagent(['ansible']) {
+                     sh 'ssh -o StrictHostKeyChecking=no ubuntu@13.201.98.131 "ls" '
+                }
+
             }
         }
 
-        stage('Build Docker Image') {
-            steps {
-                script {
-                    echo 'Building Docker image...'
-                    // Build Docker image with the tag mark_to_do:latest
-                    sh 'docker build -t krsna3629/mark_to_do:latest .'
-                }
-            }
-        }
-
-        stage('Pushing Image to DockerHub') {
-            steps {
-                // Use Jenkins credentials for Docker Hub authentication
-                withCredentials([string(credentialsId: 'DHPass', variable: 'DHPass')]) {
-                    sh '''
-                        echo $DHPass | docker login -u krsna3629 --password-stdin
-                        docker image tag mark_to_do:latest krsna3629/mark_to_do:v1.$BUILD_ID
-                        docker image push krsna3629/mark_to_do:v1.$BUILD_ID
-                        docker image push krsna3629/mark_to_do:latest
-                    '''
-                }
-            }
-        }
     }
 }
+
+
+
+
