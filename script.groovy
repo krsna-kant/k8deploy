bd96facddf965a06631582bf18b8e11835ffd942
KRSNA-KANT <krishnakantrsharma@gmail.com>
2024-10-01 16:03:44 +0530

    changed all script

--- script.groovy
+++ script.groovy
@@ -4,30 +4,33 @@ pipeline {
     stages {
         stage('Clone Code') {
             steps {
+                // Clone code from the GitHub repository
                 git branch: 'main', url: 'https://github.com/krsna-kant/K8attempt2.git'
             }
         }
+
         stage('Build Docker Image') {
             steps {
                 script {
                     echo 'Building Docker image...'
-                    // Replace the following commands with your actual Docker build and push steps
-                    sh 'docker build -t mark_to_do:latest .'  
+                    // Build Docker image with the tag mark_to_do:latest
+                    sh 'docker build -t krsna3629/mark_to_do:latest .'
                 }
             }
         }
-        stage('Pushing Image to DockerHub'){
-                steps{
+
+        stage('Pushing Image to DockerHub') {
+            steps {
+                // Use Jenkins credentials for Docker Hub authentication
                 withCredentials([string(credentialsId: 'DHPass', variable: 'DHPass')]) {
-                sh 'docker login -u krsna3629 -p ${DHPass}'
-                sh 'docker image push krsna3629/$JOB_NAME:v1.$BUILD_ID'
-                sh 'docker image push krsna3629/$JOB_NAME:latest'
-             }
-           }
+                    sh '''
+                        echo $DHPass | docker login -u krsna3629 --password-stdin
+                        docker image tag mark_to_do:latest krsna3629/mark_to_do:v1.$BUILD_ID
+                        docker image push krsna3629/mark_to_do:v1.$BUILD_ID
+                        docker image push krsna3629/mark_to_do:latest
+                    '''
+                }
+            }
         }
     }
 }
-
-
-
-
