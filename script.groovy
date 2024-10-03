854d9f7cdb4cbabd5d45b7feaf56f878df020c41
KRSNA-KANT <krishnakantrsharma@gmail.com>
2024-10-03 17:18:14 +0530

    uncommented the comment

--- script.groovy
+++ script.groovy
@@ -7,24 +7,24 @@ pipeline {
                 git branch: 'main', url: 'https://github.com/krsna-kant/K8attempt2.git'
             }
         }
-        // stage('Build Docker Image') {
-        //     steps {
-        //         script {
-        //             echo 'Building Docker image...'
-        //             // Replace the following commands with your actual Docker build and push steps
-        //             sh 'docker build -t mark_to_do:latest .'  
-        //         }
-        //     }
-        // }
-        // stage('Pushing Image to DockerHub'){
-        //         steps{
-        //         withCredentials([string(credentialsId: 'DHPass', variable: 'DHPass')]) {
-        //         sh 'docker login -u krsna3629 -p ${DHPass}'
-        //         sh 'docker image push krsna3629/$JOB_NAME:v1.$BUILD_ID'
-        //         sh 'docker image push krsna3629/$JOB_NAME:latest'
-        //      }
-        //    }
-        // }
+        stage('Build Docker Image') {
+            steps {
+                script {
+                    echo 'Building Docker image...'
+                    // Replace the following commands with your actual Docker build and push steps
+                    sh 'docker build -t mark_to_do:latest .'  
+                }
+            }
+        }
+        stage('Pushing Image to DockerHub'){
+                steps{
+                withCredentials([string(credentialsId: 'DHPass', variable: 'DHPass')]) {
+                sh 'docker login -u krsna3629 -p ${DHPass}'
+                sh 'docker image push krsna3629/$JOB_NAME:v1.$BUILD_ID'
+                sh 'docker image push krsna3629/$JOB_NAME:latest'
+             }
+           }
+        }
         stage('sending File to Ansible & K8 Servers'){
             steps{
                  sh 'scp -o StrictHostKeyChecking=no  /var/lib/jenkins/workspace/k8deployment/ansible-playbook.yml ubuntu@35.154.235.112:/home/ubuntu/'
