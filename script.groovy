efb2d31444a1c12b5e7aa391a4e14518986e3e2f
KRSNA-KANT <krishnakantrsharma@gmail.com>
2024-10-07 15:18:26 +0530

    changed ip

--- script.groovy
+++ script.groovy
@@ -27,15 +27,15 @@ pipeline {
         }
         stage('sending File to Ansible & K8 Servers'){
             steps{
-                 sh 'scp -o StrictHostKeyChecking=no  /var/lib/jenkins/workspace/k8deployment/ansible-playbook.yml ubuntu@35.154.235.112:/home/ubuntu/'
-                 sh 'scp -o StrictHostKeyChecking=no  /var/lib/jenkins/workspace/k8deployment/service.yml ubuntu@43.205.229.90:/home/ubuntu/'
-                  sh 'scp -o StrictHostKeyChecking=no  /var/lib/jenkins/workspace/k8deployment/deployment.yml ubuntu@43.205.229.90:/home/ubuntu/'
+                 sh 'scp -o StrictHostKeyChecking=no  /var/lib/jenkins/workspace/k8deployment/ansible-playbook.yml ubuntu@52.66.24.176:/home/ubuntu/'
+                 sh 'scp -o StrictHostKeyChecking=no  /var/lib/jenkins/workspace/k8deployment/service.yml ubuntu@65.1.109.57:/home/ubuntu/'
+                  sh 'scp -o StrictHostKeyChecking=no  /var/lib/jenkins/workspace/k8deployment/deployment.yml ubuntu@65.1.109.57:/home/ubuntu/'
             }
         }
         stage('Sending file to Ansible'){
             steps{
                 sshagent(['Ansible']) {
-                     sh 'ssh -o StrictHostKeyChecking=no ubuntu@35.154.235.112 "ls" '
+                     sh 'ssh -o StrictHostKeyChecking=no ubuntu@52.66.24.176 "ls" '
                 }
 
             }
@@ -43,7 +43,7 @@ pipeline {
         stage('Deploy to Kubernetes'){
             steps{
                 sshagent(['K8']){
-                    sh 'ssh -o StrictHostKeyChecking=no ubuntu@43.205.229.90 "ls" '
+                    sh 'ssh -o StrictHostKeyChecking=no ubuntu@65.1.109.57 "ls" '
                 }
             }
         }
